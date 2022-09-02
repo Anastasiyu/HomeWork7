6d55f496f8d66beed831d702271523052acca0a6
Anastasiyu <asy.safiulina@mail.ru>
2022-09-02 20:07:06 +0300

    ДЗ задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
     }
 }
